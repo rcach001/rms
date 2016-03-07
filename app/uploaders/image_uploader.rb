d5c98606e5f5cf9ee456b75257347d21b1cf5caf
Ramon <ramoncacho@gmail.com>
2016-03-06 19:43:33 -0500

    updated gemfile gem to fog, and application.rb to just require carrierwave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   if Rails.env.production?
     storage :fog
   else
-  storage :file
+    storage :file
   end
 
   # Override the directory where uploaded files will be stored.
